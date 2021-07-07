9e8e145dc0d168d4d1089090ca27f8fb56cfa360
Victor Tran <victortran415@gmail.com>
2021-07-07 13:28:23 -0700

    ADD: created a data using images that was added

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+	{
+		image: require('./images/pets/pet1.jpeg').default,
+	},
+	{
+		image: require("./images/pets/pet2.jpeg").default,
+	},
+	{
+		image: require("./images/pets/pet3.jpeg").default,
+	},
+	{
+		image: require("./images/pets/pet4.jpeg").default,
+	},
+];
