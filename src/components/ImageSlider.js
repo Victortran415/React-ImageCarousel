76717815403455acca0f3db61585465295020706
Victor Tran <victortran415@gmail.com>
2021-07-07 14:03:06 -0700

    ADD: images now switch when clicking on arrows

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -4,26 +4,31 @@ import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const ImageSlider = ({ slides }) => {
 	const [curr, setCurr] = useState(0);
-    const length = slides.length;
+	const length = slides.length;
 
-    const nextSlide = () => {
-        setCurr(curr === length - 1 ? 0 : curr + 1)
-    }
-    const prevSlide = () => {
-        setCurr(curr === 0 ? length - 1 : curr - 1)
-    }
-    console.log(curr)
-    
-    if(!Array.isArray(slides) || slides.length <= 0) {
-        return null
-    }
+	const nextSlide = () => {
+		setCurr(curr === length - 1 ? 0 : curr + 1);
+	};
+	const prevSlide = () => {
+		setCurr(curr === 0 ? length - 1 : curr - 1);
+	};
+	console.log(curr);
+
+	if (!Array.isArray(slides) || slides.length <= 0) {
+		return null;
+	}
 
 	return (
 		<section className="slider">
 			<FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
-			<FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
+			<FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
 			{SliderData.map((slide, index) => {
-				return <img src={slide.image} alt="pet images" className='image'/>;
+				return(
+				<div className={index === curr ? "slide active" : "slide"} key={index}>
+					{index === curr && (
+						<img src={slide.image} alt="pet images" className="image" />
+					)}
+				</div>)
 			})}
 		</section>
 	);
