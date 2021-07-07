eca2d55310954fbf970ddbe0c0167854bc74fdb5
Victor Tran <victortran415@gmail.com>
2021-07-07 13:55:56 -0700

    TEST: checking the icon for onClick. left arrow will decrease num, right will increase

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -4,12 +4,24 @@ import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const ImageSlider = ({ slides }) => {
 	const [curr, setCurr] = useState(0);
-	const length = slides.length;
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurr(curr === length - 1 ? 0 : curr + 1)
+    }
+    const prevSlide = () => {
+        setCurr(curr === 0 ? length - 1 : curr - 1)
+    }
+    console.log(curr)
+    
+    if(!Array.isArray(slides) || slides.length <= 0) {
+        return null
+    }
 
 	return (
 		<section className="slider">
-			<FaArrowAltCircleLeft className="left-arrow" />
-			<FaArrowAltCircleRight className="right-arrow" />
+			<FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+			<FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
 			{SliderData.map((slide, index) => {
 				return <img src={slide.image} alt="pet images" className='image'/>;
 			})}
